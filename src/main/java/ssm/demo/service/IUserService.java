fae81dd16f82e15f258ce4ba7067ad0e37afa87a
DXnm <1370131288@qq.com>
2019-04-21 16:58:19 +0800

    SSm 实现登陆

--- src/main/java/ssm/demo/service/IUserService.java
+++ src/main/java/ssm/demo/service/IUserService.java
@@ -4,5 +4,5 @@ import ssm.demo.entity.User;
 
 public interface IUserService {
     public User getUserByName(String name);
-    public boolean verify(String username,String pwd);
+    public boolean verify(String username, String pwd);
 }
